44332a9d3bdbff32c22e4b2fe1596aeef74a805a
Austin Riba <ariba@lcogt.net>
2015-11-20 10:38:44 -0800

    reorganize

--- tests/tests.py
+++ tests/tests.py
@@ -1,19 +1,28 @@
 import unittest
 import os
-from ingester import Ingester
+from ingester import Ingester, HEADER_BLACKLIST
+from utils import fits_to_dict
+
+FITS_PATH = os.path.join(
+    os.path.dirname(__file__),
+    'fits/coj1m011-kb05-20150219-0125-e90.fits'
+)
 
 
-class TestIngester(unittest.TestCase):
-    def setUp(self):
-        self.path = os.path.join(
-            os.path.dirname(__file__),
-            'fits/1.fits'
-        )
-        self.ingester = Ingester('http://localhost', 'fakebucket')
-
+class TestUtils(unittest.TestCase):
     def test_fits_to_dict(self):
-        result = Ingester.fits_to_dict(self.path)
+        result = fits_to_dict(FITS_PATH, HEADER_BLACKLIST)
+        for header in HEADER_BLACKLIST:
+            self.assertNotIn(
+                header,
+                result.keys()
+            ),
         self.assertEqual(
             'coj1m011-kb05-20150219-0125-e00.fits',
             result['ORIGNAME']
         )
+
+
+class TestIngester(unittest.TestCase):
+    def setUp(self):
+        self.ingester = Ingester('http://localhost', 'fakebucket')
