9ef40a94c0381f2305f9113ed8ccc480071cddc3
Austin Riba <ariba@lcogt.net>
2015-12-02 15:59:26 -0800

    remove moved file

--- utils.py
+++
@@ -1,9 +0,0 @@
-from astropy.io import fits
-
-
-def fits_to_dict(path, blacklist=[]):
-    hdulist = fits.open(path)
-    full_dict = dict(hdulist[0].header)
-    for header in blacklist:
-        del(full_dict[header])
-    return full_dict
