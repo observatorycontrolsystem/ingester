e413643866207b6551166b1647dd5ea3044f1b19
Austin Riba <ariba@lcogt.net>
2015-12-02 15:23:11 -0800

    dedicated settings module

--- settings.py
+++ settings.py
@@ -1,7 +1,11 @@
 import os
 
 #  General settings
+QUEUE_NAME = os.getenv('QUEUE_NAME', 'ingest_queue')
+BROKER_URL = os.getenv('BROKER_URL', 'memory://localhost')
+API_ROOT = os.getenv('API_ROOT', 'http://localhost:8000')
 
+# Fits headers we don't want to ingest
 HEADER_BLACKLIST = ['HISTORY', '']
 
 #  AWS Credentials and defaults
@@ -9,3 +13,10 @@ AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID', 'AKIAIZBYNSZZGYN3EAQQ')
 AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY', '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')
 REGION_NAME = os.getenv('REGION_NAME', 'us-west-2')
 BUCKET = os.getenv('BUCKET', 'lcogtarchivetest')
+
+# Celery Settings
+CELERY_TASK_SERIALIZER = 'json'
+CELERY_ACCEPT_CONTENT = ['json']
+CELERY_TIMEZONE = 'UTC'
+CELERY_ENABLE_UTC = True
+CELERYBEAT_SCHEDULE = {}
