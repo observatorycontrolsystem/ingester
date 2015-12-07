fc8b64aab48d6f800deea485239b55a9f8eec757
Austin Riba <ariba@lcogt.net>
2015-12-07 15:40:51 -0800

    get rid of moto, passing tests and working upload

--- tests/tests.py
+++ tests/tests.py
@@ -1,12 +1,8 @@
 import unittest
 import os
-import boto3
 import settings
-from moto import mock_s3
 from unittest.mock import patch
-from ingester.ingester import Ingester
 from tasks import do_ingest
-from ingester.utils.s3 import filename_to_s3_key
 from ingester.utils.fits import fits_to_dict
 import opentsdb_python_metrics.metric_wrappers
 opentsdb_python_metrics.metric_wrappers.test_mode = True
@@ -19,61 +15,21 @@ FITS_PATH = os.path.join(
 
 test_bucket = 'testbucket'
 
-aws_kwargs = {
-    'access_key': 'fakekey',
-    'secret_key': 'sosecure',
-    'region': 'us-west-1',
-    'bucket': test_bucket
-}
 
-
-def create_bucket():
-    s3 = boto3.resource('s3')
-    s3.create_bucket(Bucket=test_bucket)
-    bucket_versioning = s3.BucketVersioning(test_bucket)
-    bucket_versioning.enable()
-    return test_bucket
-
-
-@mock_s3
 @patch('tasks.collect_queue_length_metric')
+@patch('boto3.resource')
 class TestCelery(unittest.TestCase):
     def setUp(self):
-        create_bucket()
         settings.CELERY_ALWAYS_EAGER = True
 
-    def test_task_success(self, mock):
+    def test_task_success(self, mock, mock2):
         result = do_ingest.delay(FITS_PATH, test_bucket)
         self.assertTrue(result.successful())
-        self.assertTrue(do_ingest.delay(FITS_PATH, test_bucket).successful())
 
-    def test_task_failure(self, mock):
+    def test_task_failure(self, mock, mock2):
         result = do_ingest.delay('/pathdoesnot/exit.fits', test_bucket)
         self.assertIs(result.result.__class__, FileNotFoundError)
         self.assertTrue(result.failed())
-
-
-@mock_s3
-class TestS3(unittest.TestCase):
-    def setUp(self):
-        create_bucket()
-        s3 = boto3.resource('s3')
-        self.bucket = s3.Bucket(test_bucket)
-
-    def get_bucket_keys(self):
-        return [k.key for k in self.bucket.objects.all()]
-
-    def test_ingest_good_file(self):
-        ingester = Ingester(FITS_PATH, test_bucket)
-        ingester.ingest()
-        self.assertIn(filename_to_s3_key(FITS_PATH), self.get_bucket_keys())
-
-    def test_ingest_missing_file(self):
-        badpath = '/doesnot/exist.fits'
-        ingester = Ingester(badpath, test_bucket)
-        with self.assertRaises(FileNotFoundError):
-            ingester.ingest()
-        self.assertNotIn(filename_to_s3_key(badpath), self.get_bucket_keys())
 
 
 class TestUtils(unittest.TestCase):
