5bc2cbd633d32fbf1c81ac06879a77f2902c06dc
Austin Riba <ariba@lcogt.net>
2015-12-15 15:03:34 -0800

    update call api

--- ingester/utils/fits.py
+++ ingester/utils/fits.py
@@ -1,10 +1,20 @@
 from astropy.io import fits
+from astropy import wcs
 
 
 def fits_to_dict(f):
     hdulist = fits.open(f, mode='denywrite')
     full_dict = dict(hdulist[0].header)
     return full_dict
+
+
+def wcs_corners_from_dict(fits_dict):
+    w = wcs.WCS(fits_dict)
+    sw = w.all_pix2world(1, 1, 1)
+    sw = tuple([float(x) for x in sw])
+    ne = w.all_pix2world(fits_dict['NAXIS1'], fits_dict['NAXIS2'], 1)
+    ne = tuple([float(x) for x in ne])
+    return (sw, ne)
 
 
 def remove_headers(dictionary, blacklist):
