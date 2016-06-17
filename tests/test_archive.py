3fee5f27334405a5ea07e779f51135438af22e5d
Austin Riba <ariba@lcogt.net>
2016-06-17 02:17:01 +0000

    factor out some of the archive access stuff to modularize the code and improve tests

---
+++ tests/test_archive.py
@@ -0,0 +1,61 @@
+from unittest.mock import patch
+import unittest
+from requests.exceptions import ConnectionError, HTTPError
+
+from ingester.archive import ArchiveService
+from ingester.exceptions import NonFatalDoNotRetryError, RetryError, BackoffRetryError
+
+
+def mocked_requests_get(*args, **kwargs):
+    class MockResponse(object):
+        def __init__(self, json_data, exception):
+            self.json_data = json_data
+            self.exception = exception
+
+        def json(self):
+            return self.json_data
+
+        def raise_for_status(self):
+            if self.exception:
+                raise self.exception
+            else:
+                return None
+
+    if args[0].startswith('http://return1/'):
+        return MockResponse({'count': 1}, None)
+
+    if args[0].startswith('http://return404/'):
+        return MockResponse(None, HTTPError)
+
+    if args[0].startswith('http://badconnection/'):
+        return MockResponse(None, ConnectionError)
+
+    return MockResponse({'count': 0}, None)
+
+
+@patch('requests.get', side_effect=mocked_requests_get)
+@patch('requests.post')
+class TestArchiveService(unittest.TestCase):
+    def test_archive_post(self, post_mock, get_mock):
+        archive_service = ArchiveService(api_root='http://fake/', auth_token='')
+        archive_service.post_frame({})
+        self.assertTrue(post_mock.called)
+        self.assertEqual(post_mock.call_args[0][0], 'http://fake/frames/')
+
+    def test_existing_md5(self, post_mock, get_mock):
+        archive_service = ArchiveService(api_root='http://return1/', auth_token='')
+        with self.assertRaises(NonFatalDoNotRetryError):
+            archive_service.check_for_existing_version('')
+        self.assertFalse(post_mock.called)
+
+    def test_bad_response(self, post_mock, get_mock):
+        archive_service = ArchiveService(api_root='http://return404/', auth_token='')
+        with self.assertRaises(RetryError):
+            archive_service.check_for_existing_version('')
+        self.assertFalse(post_mock.called)
+
+    def test_bad_connection(self, post_mock, get_mock):
+        archive_service = ArchiveService(api_root='http://badconnection/', auth_token='')
+        with self.assertRaises(BackoffRetryError):
+            archive_service.check_for_existing_version('')
+        self.assertFalse(post_mock.called)
--- tests/tests.py
+++ tests/tests.py
@@ -1,14 +1,15 @@
+from unittest.mock import patch, MagicMock
+import opentsdb_python_metrics.metric_wrappers
 import unittest
 import os
 import tarfile
-from unittest.mock import patch, MagicMock
-from ingester.ingester import Ingester
-from ingester.exceptions import DoNotRetryError, NonFatalDoNotRetryError, RetryError
-import settings
-import opentsdb_python_metrics.metric_wrappers
 import dateutil
-import requests
 import hashlib
+
+from ingester.ingester import Ingester
+from ingester.exceptions import DoNotRetryError, RetryError
+import settings
+
 opentsdb_python_metrics.metric_wrappers.test_mode = True
 
 
@@ -28,23 +29,6 @@ SPECTRO_FILE = os.path.join(
     FITS_PATH,
     'KEY2014A-002_0000483537_ftn_20160119_57407.tar.gz'
 )
-
-
-def mocked_requests_get(*args, **kwargs):
-    class MockResponse:
-        def __init__(self, json_data):
-            self.json_data = json_data
-
-        def json(self):
-            return self.json_data
-
-        def raise_for_status(self):
-            return None
-
-    if args[0].startswith('http://return1/'):
-        return MockResponse({'count': 1})
-
-    return MockResponse({'count': 0})
 
 
 def mocked_s3_object(*args, **kwargs):
@@ -74,15 +58,14 @@ def mock_hashlib_md5(*args, **kwargs):
 
 
 @patch('boto3.resource', side_effect=mocked_s3_object)
-@patch('requests.post')
 class TestIngester(unittest.TestCase):
     def setUp(self):
         hashlib.md5 = MagicMock(side_effect=mock_hashlib_md5)
-        requests.get = MagicMock(side_effect=mocked_requests_get)
         fits_files = [os.path.join(FITS_PATH, f) for f in os.listdir(FITS_PATH)]
+        self.archive_mock = MagicMock()
         self.ingesters = [
             Ingester(
-                path, 'testbucket', 'http://testendpoint', auth_token='',
+                path, 'testbucket', archive_service=self.archive_mock,
                 required_headers=settings.REQUIRED_HEADERS,
                 blacklist_headers=settings.HEADER_BLACKLIST,
                 )
@@ -93,132 +76,116 @@ class TestIngester(unittest.TestCase):
         ingester = Ingester(
             path,
             'test_bucket',
-            'http://testendpoint',
-            auth_token='',
+            archive_service=self.archive_mock,
             blacklist_headers=settings.HEADER_BLACKLIST,
             required_headers=settings.REQUIRED_HEADERS
         )
         return ingester
 
-    def test_ingest_file(self, requests_mock, s3_mock):
+    def test_ingest_file(self, s3_mock):
         for ingester in self.ingesters:
             ingester.ingest()
             self.assertTrue(s3_mock.called)
-            self.assertTrue(requests_mock.called)
+            self.assertTrue(self.archive_mock.post_frame.called)
 
-    def test_missing_file(self, requests_mock, s3_mock):
+    def test_missing_file(self, s3_mock):
         ingester = self.create_ingester_for_path('/path/doesnot/exist.fits.fz')
         with self.assertRaises(RetryError):
             ingester.ingest()
         self.assertFalse(s3_mock.called)
-        self.assertFalse(requests_mock.called)
+        self.assertFalse(self.archive_mock.post_frame.called)
 
-    def test_required(self, requests_mock, s3_mock):
+    def test_required(self, s3_mock):
         ingester = Ingester(
             FITS_FILE,
             'test_bucket',
-            'http://testendpoint',
-            auth_token='',
+            archive_service=self.archive_mock,
             blacklist_headers=settings.HEADER_BLACKLIST,
             required_headers=['fooheader']
         )
         with self.assertRaises(DoNotRetryError):
             ingester.ingest()
         self.assertFalse(s3_mock.called)
-        self.assertFalse(requests_mock.called)
+        self.assertFalse(self.archive_mock.post_frame.called)
 
-    def test_get_area(self, requests_mock, s3_mock):
+    def test_get_area(self, s3_mock):
         ingester = self.create_ingester_for_path(FITS_FILE)
         ingester.ingest()
-        self.assertEqual('Polygon', requests_mock.call_args[1]['json']['area']['type'])
+        self.assertEqual('Polygon', self.archive_mock.post_frame.call_args[0][0]['area']['type'])
         ingester = self.create_ingester_for_path(CAT_FILE)
         ingester.ingest()
-        self.assertIsNone(requests_mock.call_args[1]['json']['area'])
+        self.assertIsNone(self.archive_mock.post_frame.call_args[0][0]['area'])
 
-    def test_blacklist(self, requests_mock, s3_mock):
+    def test_blacklist(self, s3_mock):
         ingester = Ingester(
             FITS_FILE,
             'test_bucket',
-            'http://testendpoint',
-            auth_token='',
+            archive_service=self.archive_mock,
             blacklist_headers=['DAY-OBS', '', 'COMMENT', 'HISTORY'],
             required_headers=settings.REQUIRED_HEADERS
         )
         ingester.ingest()
-        self.assertNotIn('DAY-OBS', requests_mock.call_args[1]['json'].keys())
+        self.assertNotIn('DAY-OBS', self.archive_mock.post_frame.call_args[0][0].keys())
 
-    def test_reduction_level(self, requests_mock, s3_mock):
+    def test_reduction_level(self, s3_mock):
         for ingester in self.ingesters:
             ingester.ingest()
-            self.assertIn('RLEVEL', requests_mock.call_args[1]['json'].keys())
+            self.assertIn('RLEVEL', self.archive_mock.post_frame.call_args[0][0].keys())
 
-    def test_related(self, requests_mock, s3_mock):
+    def test_related(self, s3_mock):
         ingester = self.create_ingester_for_path(FITS_FILE)
         ingester.ingest()
         self.assertEqual(
             'bias_kb05_20150219_bin2x2',
-            requests_mock.call_args[1]['json']['L1IDBIAS']
+            self.archive_mock.post_frame.call_args[0][0]['L1IDBIAS']
         )
         self.assertEqual(
             'dark_kb05_20150219_bin2x2',
-            requests_mock.call_args[1]['json']['L1IDDARK']
+            self.archive_mock.post_frame.call_args[0][0]['L1IDDARK']
         )
         self.assertEqual(
             'flat_kb05_20150219_SKYFLAT_bin2x2_V',
-            requests_mock.call_args[1]['json']['L1IDFLAT']
+            self.archive_mock.post_frame.call_args[0][0]['L1IDFLAT']
         )
 
-    def test_catalog_related(self, requests_mock, s3_mock):
+    def test_catalog_related(self, s3_mock):
         ingester = self.create_ingester_for_path(CAT_FILE)
         ingester.ingest()
         self.assertEqual(
             'cpt1m010-kb70-20151219-0073-e10',
-            requests_mock.call_args[1]['json']['L1IDCAT']
+            self.archive_mock.post_frame.call_args[0][0]['L1IDCAT']
         )
 
-    def test_spectograph(self, requests_mock, s3_mock):
+    def test_spectograph(self, s3_mock):
         ingester = self.create_ingester_for_path(SPECTRO_FILE)
         ingester.ingest()
-        self.assertEqual(90, requests_mock.call_args[1]['json']['RLEVEL'])
-        self.assertTrue(dateutil.parser.parse(requests_mock.call_args[1]['json']['L1PUBDAT']))
+        self.assertEqual(90, self.archive_mock.post_frame.call_args[0][0]['RLEVEL'])
+        self.assertTrue(dateutil.parser.parse(self.archive_mock.post_frame.call_args[0][0]['L1PUBDAT']))
 
-    def test_spectrograph_missing_meta(self, requests_mock, s3_mock):
+    def test_spectrograph_missing_meta(self, s3_mock):
         tarfile.TarFile.getnames = MagicMock(return_value=[''])
         ingester = self.create_ingester_for_path(SPECTRO_FILE)
         with self.assertRaises(DoNotRetryError):
             ingester.ingest()
 
-    def test_empty_string_for_na(self, requests_mock, s3_mock):
+    def test_empty_string_for_na(self, s3_mock):
         ingester = self.create_ingester_for_path(
             os.path.join(FITS_PATH, 'coj1m011-fl08-20151216-0049-b00.fits')
         )
         ingester.ingest()
-        self.assertFalse(requests_mock.call_args[1]['json']['OBJECT'])
-        self.assertTrue(requests_mock.call_args[1]['json']['DATE-OBS'])
+        self.assertFalse(self.archive_mock.post_frame.call_args[0][0]['OBJECT'])
+        self.assertTrue(self.archive_mock.post_frame.call_args[0][0]['DATE-OBS'])
 
-    def test_existing_version(self, requests_mock, s3_mock):
-        requests.get = MagicMock(side_effect=mocked_requests_get)
-        ingester = Ingester(
-            FITS_FILE,
-            'test_bucket',
-            'http://return1/',
-            auth_token='',
-            blacklist_headers=settings.HEADER_BLACKLIST,
-            required_headers=settings.REQUIRED_HEADERS
-        )
-        with self.assertRaises(NonFatalDoNotRetryError):
-            ingester.ingest()
-
-    def test_s3_get(self, requests_mock, s3_mock):
+    def test_s3_get(self, s3_mock):
         ingester = self.create_ingester_for_path('s3://testbucket/testfile.fits')
         ingester.ingest()
         self.assertTrue(s3_mock.called)
-        self.assertTrue(requests_mock.called)
+        self.assertTrue(self.archive_mock.post_frame.called)
 
-    def test_reqnum_null_or_int(self, requests_mock, s3_mock):
+    def test_reqnum_null_or_int(self, s3_mock):
         for ingester in self.ingesters:
             ingester.ingest()
-            reqnum = requests_mock.call_args[1]['json']['REQNUM']
+            reqnum = self.archive_mock.post_frame.call_args[0][0]['REQNUM']
             try:
                 self.assertIsNone(reqnum)
             except AssertionError:
