1e6ba2cc2130961c5e3b1e4a507d0b543e9bb63b
Austin Riba <austin@m51.io>
2019-10-15 11:40:40 -0700

    Updates to the ingest_frame script

--- scripts/ingest_frame.py
+++ scripts/ingest_frame.py
@@ -1,14 +1,23 @@
 #!/bin/env python3
 import argparse
 import logging
+import sys
 
 from ingester.ingester import frame_exists, upload_file_and_ingest_to_archive
+from ingester.exceptions import NonFatalDoNotRetryError
 
 logger = logging.getLogger('ingester')
+logger.setLevel(logging.CRITICAL)  # silence logging for command line tool
+
+description = (
+    'Upload a file to the LCO archive. This script will output the resulting URL '
+    'if the upload is successful. An optional flag --check-only can be used to '
+    'check for the existence of a file without uploading it (based on md5).'
+)
 
 
 def main():
-    parser = argparse.ArgumentParser()
+    parser = argparse.ArgumentParser(description=description)
     parser.add_argument('path', help='Path to file')
     parser.add_argument('--api-root', help='API root')
     parser.add_argument('--auth-token', help='API token')
@@ -19,10 +28,21 @@ def main():
 
     if args.check_only:
         exists = frame_exists(args.path, api_root=args.api_root, auth_token=args.auth_token)
-        logger.info(exists)
-        return int(not exists)
+        sys.stdout.write(str(exists))
+        sys.exit(int(not exists))
 
-    return upload_file_and_ingest_to_archive(**vars(args))
+    try:
+        result = upload_file_and_ingest_to_archive(**vars(args))
+    except NonFatalDoNotRetryError:
+        sys.stdout.write('File already exists in archive.')
+        sys.exit(1)
+    except Exception as e:
+        sys.stdout.write('Exception uploading file: ')
+        sys.stdout.write(str(e))
+        sys.exit(1)
+
+    sys.stdout.write(result['url'])
+    sys.exit(0)
 
 
 if __name__ == '__main__':
