983e5d1bf354d304999b1dc2e04b7f96b991649d
Jon <jnation@lco.global>
2019-10-17 00:50:23 +0000

    review comments

--- settings/settings.py
+++ settings/settings.py
@@ -9,19 +9,19 @@ AUTH_TOKEN = os.getenv('AUTH_TOKEN', '')
 BUCKET = os.getenv('BUCKET', 'ingestertest')
 
 # Files we wish to ignore
-IGNORED_CHARS = os.getenv('IGNORED_CHARS', "-t00,-x00,-g00,-l00,-kb11,-kb15,tstnrs").split(',')
+IGNORED_CHARS = os.getenv('IGNORED_CHARS', "-t00,-x00,-g00,-l00,-kb11,-kb15,tstnrs").strip(',').replace(' ','').split(',')
 
 # Fits headers we don't want to ingest
-HEADER_BLACKLIST = os.getenv('HEADER_BLACKLIST', "HISTORY,COMMENT,").split(',')
+HEADER_BLACKLIST = os.getenv('HEADER_BLACKLIST', "HISTORY,COMMENT,").strip(',').replace(' ','').split(',')
 
 # Fits headers that must be present
-REQUIRED_HEADERS = os.getenv('REQUIRED_HEADERS', "PROPID,DATE-OBS,INSTRUME,SITEID,TELID,OBSTYPE,BLKUID").split(',')
+REQUIRED_HEADERS = os.getenv('REQUIRED_HEADERS', "PROPID,DATE-OBS,INSTRUME,SITEID,TELID,OBSTYPE,BLKUID").strip(',').replace(' ','').split(',')
 
 # Calibration observation types (OBSTYPE)
-CALIBRATION_TYPES = os.getenv('CALIBRATION_TYPES', "BIAS,DARK,SKYFLAT,EXPERIMENTAL").split(',')
+CALIBRATION_TYPES = os.getenv('CALIBRATION_TYPES', "BIAS,DARK,SKYFLAT,EXPERIMENTAL").strip(',').replace(' ','').split(',')
 
 # Proposals including these strings will be considered public data
-PUBLIC_PROPOSALS = os.getenv('PUBLIC_PROPOSALS', "EPO,calib,standard,pointing").split(',')
+PUBLIC_PROPOSALS = os.getenv('PUBLIC_PROPOSALS', "EPO,calib,standard,pointing").strip(',').replace(' ','').split(',')
 
 # Crawler RabbitMQ Exchange Name
 CRAWLER_EXCHANGE_NAME = os.getenv('CRAWLER_EXCHANGE_NAME', 'fits_files')
