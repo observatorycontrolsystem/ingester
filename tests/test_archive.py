d2de3954a5891d7d020b8be8935c96e53cdb0482
Matt Daily <mdaily@lco.global>
2020-03-04 15:26:44 -0800

    Add HTTP status code to MockResponse class. Assert DoNotRetryError on 4xx responses

--- tests/test_archive.py
+++ tests/test_archive.py
@@ -5,14 +5,15 @@ from requests.exceptions import ConnectionError, HTTPError
 
 from lco_ingester.archive import PostProcService
 from lco_ingester.archive import ArchiveService
-from lco_ingester.exceptions import NonFatalDoNotRetryError, RetryError, BackoffRetryError
+from lco_ingester.exceptions import BackoffRetryError, DoNotRetryError
 
 
 def mocked_requests_get(*args, **kwargs):
     class MockResponse(object):
-        def __init__(self, json_data, exception):
+        def __init__(self, json_data, exception, status_code):
             self.json_data = json_data
             self.exception = exception
+            self.status_code = status_code
 
         def json(self):
             return self.json_data
@@ -24,15 +25,15 @@ def mocked_requests_get(*args, **kwargs):
                 return None
 
     if args[0].startswith('http://return1/'):
-        return MockResponse({'count': 1}, None)
+        return MockResponse({'count': 1}, None, 400)
 
     if args[0].startswith('http://return404/'):
-        return MockResponse(None, HTTPError)
+        return MockResponse(None, HTTPError, 404)
 
     if args[0].startswith('http://badconnection/'):
-        return MockResponse(None, ConnectionError)
+        return MockResponse(None, ConnectionError, 500)
 
-    return MockResponse({'count': 0}, None)
+    return MockResponse({'count': 0}, None, 200)
 
 
 @patch('requests.get', side_effect=mocked_requests_get)
@@ -61,7 +62,7 @@ class TestArchiveService(unittest.TestCase):
 
     def test_bad_response(self, post_mock, get_mock):
         archive_service = ArchiveService(api_root='http://return404/', auth_token='', broker_url='')
-        with self.assertRaises(RetryError):
+        with self.assertRaises(DoNotRetryError):
             archive_service.version_exists('')
         self.assertFalse(post_mock.called)
 
